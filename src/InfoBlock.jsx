4f4debeefa28370766218d121f4d9d5b51623832
Grayson Bertaina <grayaerialvisual@gmail.com>
2021-05-15 17:41:48 -0400

    test1

--- src/InfoBlock.jsx
+++ src/InfoBlock.jsx
@@ -33,7 +33,7 @@ const styles = (theme) => ({
   },
   formControl: {
     margin: theme.spacing(1),
-    minWidth: 121,
+    minWidth: 120,
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'center',
