7f177dfdcafa94e3f4e271d776d5b4a6aebfab5f
Grayson Bertaina <grayaerialvisual@gmail.com>
2021-05-16 09:05:32 -0400

    search result ui

--- src/SearchResult.jsx
+++ src/SearchResult.jsx
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
+import Slider from '@material-ui/core/Slider';
 
 const styles = (theme) => ({
 	searchReturned: {
@@ -10,9 +12,19 @@ const styles = (theme) => ({
 		justifyContent: 'center',
 		padding: '2vh',
 	},
+	COVIDreport: {
+		display: 'flex',
+		justifyContent: 'center',
+		flexDirection: 'column',
+		padding: '2vh',
+	},
 	innerSearch: {
 		width: '90%',
 	},
+	root: {
+    width: '100%',
+    background: 'linear-gradient(45deg, #53a6ff 30%, #ff5353 90%)',
+  },
 });
 
 
@@ -37,10 +49,25 @@ class SearchResult extends React.Component {
 						<Button variant="contained" color="primary" onClick={this.props.changeState}>Back To Search</Button>
 
 					</div>
+
 				</div>
 				<div className={classes.COVIDreport}>
-					<h1>COVID, Security, and Analysis</h1>
+
+
+				<h1>COVID, Security, and Analysis</h1>
+					<div class={classes.verticalSection}>
+						<div className={classes.marginSlider} />
+							<Typography gutterBottom>Covid Risk</Typography>
+							<PrettoSlider className={classes.root} valueLabelDisplay="auto" aria-label="pretto slider" defaultValue={20} disabled/>
+						<div className={classes.marginSlider} />
+							<Typography gutterBottom>Homicide Index</Typography>
+							<PrettoSlider className={classes.root} valueLabelDisplay="auto" aria-label="pretto slider" defaultValue={20} disabled/>
+						<div className={classes.marginSlider} />
+							<Typography gutterBottom>Corruption Index</Typography>
+							<PrettoSlider className={classes.root} valueLabelDisplay="auto" aria-label="pretto slider" defaultValue={20} disabled/>
+					</div>
 				</div>
+
 			</div>
 
 
@@ -49,6 +76,45 @@ class SearchResult extends React.Component {
 	}
 }
 
+const PrettoSlider = withStyles({
+	root: {
+
+		height: 8,
+	},
+	thumb: {
+		height: 34,
+		width: 34,
+		backgroundColor: '#fff',
+		border: '4px solid currentColor',
+		marginTop: -8,
+		marginLeft: -12,
+		'&:focus, &:hover, &$active': {
+			boxShadow: 'inherit',
+		},
+	},
+	active: {},
+	valueLabel: {
+		left: 'calc(-50% + 4px)',
+	},
+	track: {
+		height: '3vh',
+		borderRadius: 4,
+	},
+	rail: {
+		height: '3vh',
+		borderRadius: 4,
+	},
+})(Slider);
+
+function ValueLabelComponent(props) {
+  const { children, open, value } = props;
+
+  return (
+    <Tooltip open={open} enterTouchDelay={0} placement="top" title={value}>
+      {children}
+    </Tooltip>
+  );
+}
 
 SearchResult.propTypes = {
   classes: PropTypes.object.isRequired
