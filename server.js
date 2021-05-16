892dd0125d5600ce0ebcab205b34a41a9dd8ec03
aaroexxt <aaron.becker@gmail.com>
2021-05-16 08:53:57 -0400

    BackEnD

--- server.js
+++ server.js
@@ -3,12 +3,17 @@ const path = require('path');
 const app = express();
 const http = require('http').Server(app);
 const fetch = require('node-fetch');
+const csv = require('csv-parser');
+const fs = require('fs');
 
 const cwd = __dirname;
 const RequestHandler = require("./drivers/RequestHandler.js");
 const asyncMiddleware = require("./drivers/asyncMiddleware.js");
 
 const {ICAOtoIATA, IATAtoICAO, airportSearch} = require("./drivers/airportSearch.js");
+const countries = JSON.parse(fs.readFileSync("./data/countries.json"));
+const stripBom = require('strip-bom-stream');
+const {ratcliffObershelp, levenshtein} = require("./drivers/stringCompare.js");
 
 const settings = require("./data/settings.json");
 
@@ -101,7 +106,7 @@ const dateFormat = dateStr => {
 
 const processFlightSearchResponse = (resp, roundTrip) => {
 	/*
-		Array of offer
+		Array of offers
 
 		Each offer:
 		DONE - Departure/arrival airport
@@ -262,8 +267,6 @@ FLrouter.get("/searchRoundtrip/:origin/:destination/:seatClass/:dateDeparture/:d
 	})
 }))
 
-app.use("/api/map", MAProuter);
-app.use("/api/flights", FLrouter);
 app.get("/api/search/:text", (req, res) => {
 	let search = airportSearch(req.params.text);
 
@@ -274,6 +277,156 @@ app.get("/api/search/:text", (req, res) => {
 		res.end("Nothing found");
 	}
 })
+
+var covidData = [];
+let covidDataReady = false;
+fs.createReadStream(path.join(process.cwd(),settings.covidDatapath))
+	.pipe(stripBom())
+	.pipe(csv())
+	.on('data', (data) => covidData.push(data))
+	.on('end', () => {
+		covidDataReady = true;
+		console.log("DATALOAD: covid");
+	});
+
+var homicideData = [];
+let homicideDataReady = false;
+fs.createReadStream(path.join(process.cwd(),settings.homicideDatapath))
+	.pipe(stripBom())
+	.pipe(csv({ separator: ',' }))
+	.on('data', (data) => homicideData.push(data))
+	.on('end', () => {
+		homicideDataReady = true;
+		console.log("DATALOAD: homicides");
+	});
+
+var corruptionData = [];
+let corruptionDataReady = false;
+fs.createReadStream(path.join(process.cwd(),settings.corruptionDatapath))
+	.pipe(stripBom())
+	.pipe(csv({ separator: ',' }))
+	.on('data', (data) => corruptionData.push(data))
+	.on('end', () => {
+		corruptionDataReady = true;
+		console.log("DATALOAD: corruption");
+	});
+
+//This is also a jank hack pls fix :(
+const expandCountry = country => {
+	if (country.length == 2) {
+		for (let i=0; i<countries.length; i++) {
+			let cTest = countries[i];
+			if (cTest.code == country) {
+				return cTest.name;
+			}
+		}
+	}
+	return country;
+}
+
+const threshold = 0.6;
+const hBounds = [2001, 2016];
+const getHomicideData = (country) => {
+	country = expandCountry(country);
+	let bestMatch = [0, {}]
+	for (let i=0; i<homicideData.length; i++) {
+		let matchV = ratcliffObershelp(homicideData[i]["UNODC Name"], country);
+		if (matchV > bestMatch[0]) {
+			bestMatch[0] = matchV;
+			bestMatch[1] = homicideData[i];
+		}
+	}
+
+	if (bestMatch[0] > threshold) {
+		for (let j=hBounds[1]; j>=hBounds[0]; j++) {
+			let dataT = String(bestMatch[1][j]).replace(/^\s+|\s+$/g, ''); //strip extra chars
+			if (!isNaN(Number(dataT))) {
+				return dataT;
+			}
+		}
+	}
+	return null;
+}
+
+const getCorruptionData = (country) => {
+	country = expandCountry(country);
+	let bestMatch = [0, {}]
+	for (let i=0; i<corruptionData.length; i++) {
+		let matchV = ratcliffObershelp(corruptionData[i]["Country"], country);
+		if (matchV > bestMatch[0]) {
+			bestMatch[0] = matchV;
+			bestMatch[1] = corruptionData[i];
+		}
+	}
+
+	if (bestMatch[0] > threshold) {
+		let dataT = String(bestMatch[1]["CPI2015"]).replace(/^\s+|\s+$/g, ''); //strip extra chars
+		if (!isNaN(Number(dataT))) {
+			return 100-dataT;
+		}
+	}
+	return null;
+}
+
+const getCovidData = (country, city, state) => {
+	country = expandCountry(country);
+	let matches = [];
+	for (let i=0; i<covidData.length; i++) {
+		let r = covidData[i];
+		//PEOPLE_POSITIVE_CASES_COUNT,COUNTY_NAME,PROVINCE_STATE_NAME,REPORT_DATE,CONTINENT_NAME,DATA_SOURCE_NAME,PEOPLE_DEATH_NEW_COUNT,COUNTY_FIPS_NUMBER,COUNTRY_ALPHA_3_CODE,COUNTRY_SHORT_NAME,COUNTRY_ALPHA_2_CODE,PEOPLE_POSITIVE_NEW_CASES_COUNT,PEOPLE_DEATH_COUNT
+		//0,Clarke,Alabama,2020-01-21,America,New York Times,0,01025,USA,United States,US,0,0
+		let date = r["REPORT_DATE"];
+		let cName = r["COUNTRY_SHORT_NAME"];
+		let citName = r["COUNTY_NAME"];
+		let statName = r["PROVINCE_STATE_NAME"];
+		let cases = r["PEOPLE_POSITIVE_CASES_COUNT"];
+
+		let matchV = ratcliffObershelp(cName, country);
+		if (matchV > threshold) {
+			let nMatchV = 0;
+			nMatchV += ratcliffObershelp(citName, city);
+			nMatchV += ratcliffObershelp(statName, state);
+			nMatchV /= 2;
+			if (nMatchV > threshold) {
+				matches.push(r);
+			}
+		}
+	}
+
+	let newestData = [0, {}];
+	for (let i=0; i<matches.length; i++) {
+		let time = new Date(matches[i]["REPORT_DATE"]).getTime();
+		if (time > newestData[0]) {
+			newestData[0] = time;
+			newestData[1] = matches[i];
+		}
+	}
+
+	if (newestData[0] == 0) {
+		return null;
+	} else {
+		return newestData[1]["PEOPLE_POSITIVE_CASES_COUNT"];
+	}
+}
+
+app.get("/api/risk/:country/:city/:state", (req, res) => {
+	let city = req.params.city || "";
+	let state = req.params.state || "";
+	let country = req.params.country;
+
+	let homicideRisk = homicideDataReady ? getHomicideData(country) : 100;
+	let corruptionRisk = corruptionDataReady ? getCorruptionData(country) : 100;
+	let covidRisk = covidDataReady ? getCovidData(country, city, state)/1000 : 100;
+
+	return res.end(JSON.stringify({
+		homicide: homicideRisk,
+		corruption: corruptionRisk,
+		covidRisk: covidRisk
+	}));
+})
+
+app.use("/api/map", MAProuter);
+app.use("/api/flights", FLrouter);
 app.use("/status", (req, res) => {
 	return res.end("OK");
 })
